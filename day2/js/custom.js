d4fd90dfac708b90c33a78373c7632f76c05a536
parvvee <parvvee@gmail.com>
2017-04-05 20:04:31 +0530

    second day commit

---
+++ day2/js/custom.js
@@ -0,0 +1,5 @@
+$(".sample-content").pxgradient({
+  step: 10, // Step Color. The smaller the number, the greater the load. Default: 10
+  colors: ["#fc0","#0fc","#00f"], // an array of hex colors.
+  dir: "x" // gradient direction. "x" - horizontal,  "y" - vertical
+});
