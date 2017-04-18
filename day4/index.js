daf854a19c66e062677c630ae2d660875869dd4c
parvvee <parvvee@gmail.com>
2017-04-18 19:44:23 +0530

    day 4 commit

---
+++ day4/index.js
@@ -0,0 +1,89 @@
+  var a =  document.getElementById("dummy_id");
+
+
+  
+//
+//   a.addEventListener("mouseenter", function( event ){
+//
+//           console.log("mouse entered in to dummy_123 class");
+//             a.style.backgroundColor = 'red';
+//         var content = document.createTextNode("<p>vinod seems to be sleepy</p>");
+//
+//               a.appendChild(content);
+// });
+//
+// document.getElementById("dummy_id").addEventListener("mouseleave", function( event ){
+//     a.style.backgroundColor = 'white';
+//         console.log("mouse left dummy_123 class");
+// });
+//
+//
+// //
+// // (function test() {
+// //
+// //
+// //
+// // var a = "this is test";
+// // console.log(a+a+a);
+// // }
+// //
+// //
+// //
+// // )();
+//
+// //
+// //
+// //  variables in depth
+//
+// //java
+// //
+// //class
+// //
+// // methods
+// //
+// //
+// //
+// //
+// //b = new class();
+// //b.method;
+//
+//
+// myvar1 = new Object();
+//
+// myvar1.demo = function(){
+//
+//       return 'crazy console stuff' ;
+//
+// };
+//
+// myvar = {
+//
+//
+//
+//     demo : function(a, b, c) {
+//
+//
+//         return a * b * c;
+//
+//     },
+//
+//     demo2 : function(a,b,c , d) {
+//
+//             return this.demo(a,b,c) + d;
+//
+//     }
+//
+// }
+//
+//
+// console.log(myvar.demo2(4,5,6,10))
+//
+// //console.log(myvar.demo(4,5,6));
+//
+// //console.log(myvar1.demo());
+//
+// myvar = {
+//       b: 'test',
+//
+// };
+// //console.log(typeof(myvar.valueOf()));
